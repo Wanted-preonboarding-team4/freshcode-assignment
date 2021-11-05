161732708fbc1995bbd4617f5547e207cabe7009
Sungeheon <elon4856@gmail.com>
2021-11-06 03:28:00 +0900

    feat: jwt를 통한 권한인증 확인 기능 구현.

--- app/view/product_view.py
+++ app/view/product_view.py
@@ -3,18 +3,24 @@ from app.models import MenuRegister
 from sqlalchemy.orm import Session
 from app.database.conn import db
 from starlette.responses import JSONResponse
-from app.service.product_service import is_menu_name_exist,is_menu_exist
+from app.service.product_service import is_menu_name_exist, is_menu_exist_than_update_product, delete_product_if_exist
+from typing import AnyStr, Any, Dict
+from app.utils import JWTBearer,JWTBearerForAdminOnly
+
 
 router = APIRouter(prefix="/products")
 
+JSONObject = Dict[AnyStr, Any]
 
-@router.post("/create")
-def create_product(menu_info: MenuRegister, session: Session = Depends(db.session)):
-    if not menu_info.category_id or not menu_info.menu_name or not menu_info.description or not menu_info.item:
+
+@router.post("/create", dependencies=[Depends(JWTBearerForAdminOnly())])
+def create_product(menu_info: MenuRegister, session: Session = Depends(db.session) ):
+    if not menu_info.category_id or not menu_info.name or not menu_info.description or not menu_info.item:
         return JSONResponse(status_code=400, content=dict(msg="빠지면 안되는 항목이 있습니다. 모두 추가해주세요."))
     if is_menu_name_exist(menu_info, session):
         return JSONResponse(status_code=400, content=dict(msg="이미 존재하는 메뉴입니다."))
-    return JSONResponse(status_code=201, content=dict(msg=f"{menu_info.menu_name} 생성 성공하였습니다."))
+    return JSONResponse(status_code=201, content=dict(msg=f"{menu_info.name} 생성 성공하였습니다."))
+
 
 #
 # @router.patch("/{menu_id}")
